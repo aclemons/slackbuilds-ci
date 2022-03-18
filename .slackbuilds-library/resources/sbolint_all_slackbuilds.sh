92dfd47be94dfa4358cac1b40866966cd785bb35
Andrew Clemons <andrew.clemons@gmail.com>
2022-03-18 18:43:16 +1300

    Enable other disabled sbolints now that master is clean.

--- .slackbuilds-library/resources/sbolint_all_slackbuilds.sh
+++ .slackbuilds-library/resources/sbolint_all_slackbuilds.sh
@@ -7,17 +7,10 @@ mkdir -p log
 
 # shellcheck disable=SC2016
 find . -name '*.SlackBuild' -printf '%P\n' | xargs -I xx dirname xx | parallel --halt 0 -q -I xx bash -c 'mkdir -p "log/xx" && perl sbolint -q "xx" > "log/xx/$(basename xx)"' || true
-find log -empty -type f -delete
-find log -empty -type d -delete
 
 # clear some things for now
 find log -type f | parallel -q sed -i '/xmind: WARN: README has lines >72 characters/d'
 find log -type f | parallel -q sed -i '/fuse-overlayfs: WARN: README has lines >72 characters/d'
-find log -type f | parallel -q sed -i '/template comment should be removed/d'
-find log -type f | parallel -q sed -i '/doinst.sh exists, but the script doesn/d'
-find log -type f | parallel -q sed -i '/script installs doinst.sh, but it does/d'
-find log -type f | parallel -q sed -i '/VERSION.*echo.*doesn.t match VERSION in the .info file/d'
-find log -type f | parallel -q sed -i '/VERSION ignores environment, try .*echo./d'
 
 find log -empty -type f -delete
 find log -empty -type d -delete
