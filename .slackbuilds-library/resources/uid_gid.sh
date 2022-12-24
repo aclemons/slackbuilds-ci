2ed92546a0d3f774dae2c7b5c24ce3fda68596b8
Andrew Clemons <andrew.clemons@gmail.com>
2022-12-24 12:46:28 +0900

    Lint uid_gig.sh.

--- .slackbuilds-library/resources/uid_gid.sh
+++ .slackbuilds-library/resources/uid_gid.sh
@@ -2,24 +2,24 @@
 
 set -u
 
-grep Group: uid_gid.txt | egrep -v -- "(--|TBA)" | sort |
-while read line; do
+grep Group: uid_gid.txt | grep -E -v -- "(--|TBA)" | sort |
+while read -r line; do
   name=$( awk '{print $2}' <<<"$line" )
   gid=$( awk '{print $4}' <<<"$line" )
 
-  if ! getent group $name >/dev/null; then
-    groupadd -g $gid $name
+  if ! getent group "$name" >/dev/null; then
+    groupadd -g "$gid" "$name"
   fi
 done
 
-grep User: uid_gid.txt | egrep -v -- "(--|TBA)" | sort |
-while read line; do
+grep User: uid_gid.txt | grep -E -v -- "(--|TBA)" | sort |
+while read -r line; do
   name=$( awk '{print $2}' <<<"$line" )
   uid=$( awk '{print $4}' <<<"$line" )
   gid=$( awk '{print $6}' <<<"$line" )
 
-  if ! getent passwd $name >/dev/null; then
-    useradd -u $uid -g $gid $name
+  if ! getent passwd "$name" >/dev/null; then
+    useradd -u "$uid" -g "$gid" "$name"
   fi
 done
 
